39a1838a6bdf4c5e51f641490d1300f837246d36
kim_ck <kim_ck@commerce21.co.jp>
2025-03-16 19:09:43 +0900

    init

---
+++ src/main/java/com/thymeleaf/study/boot_thymleaf/BootThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.study.boot_thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootThymleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootThymleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thymeleaf/study/boot_thymleaf/BootThymleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.study.boot_thymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootThymleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
